cb16433e0adca31afabd8855f0e8db09294f26da
xjflyttp <xjflyttp@gmail.com>
2015-03-06 16:17:07 +0800

    update

--- PNotifyAssets.php
+++ PNotifyAssets.php
@@ -7,16 +7,10 @@ use yii\web\AssetBundle;
 
 class PNotifyAssets extends AssetBundle {
 
-    public $sourcePath;
+    public $sourcePath = '@vendor/xj/yii2-pnotify-widget/assets';
     public $basePath = '@webroot/assets';
-    public $publishOptions = ['forceCopy' => YII_DEBUG];
     public $js = ['pnotify.custom.min.js'];
     public $css = ['pnotify.custom.min.css'];
     public $depends = ['yii\web\JqueryAsset'];
-
-    public function init() {
-        $this->sourcePath = dirname(__FILE__) . DIRECTORY_SEPARATOR . 'assets';
-        return parent::init();
-    }
 
 }
